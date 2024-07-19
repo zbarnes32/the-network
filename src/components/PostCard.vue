c4a680dae5d8b87c73247ad19a22821a8dd7efc7
Zach Barnes <zachary.barnes.32@gmail.com>
2024-07-19 11:51:38 -0600

    Getting ID's to post on the page for testing purposes.

---
+++ src/components/PostCard.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Post } from '../models/Post.js';
+
+
+
+defineProps({
+    postProp: { type: Post, required: true}
+})
+
+</script>
+
+
+<template>
+<p>{{  postProp.id }}</p>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,6 +3,7 @@ import { computed, onMounted } from 'vue';
 import Pop from '../utils/Pop.js';
 import { postsService } from '../services/PostsService.js';
 import { AppState } from '../AppState.js';
+import PostCard from '../components/PostCard.vue';
 
 const posts = computed(() => AppState.posts)
 
@@ -25,10 +26,8 @@ async function getPosts(){
       <div class="col-2 sidebar justify-content-start">
         <p>Side goes here</p>
       </div>
-      <div class="col-8">
-        <p>Post go here</p>
-        {{ posts }}
-      </div>
+      <div v-for="post in posts" :key="post.id" class="col-md-8">
+       <PostCard :postProp="post" /></div>
       <div class="col-2">
         <p>Ads go here</p>
       </div>
